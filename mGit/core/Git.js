fc03f1e7730086852565f61ffb0ae36977bbb2ed
ASHISH DULHANI <ashishdulhani@novopay.in>
2018-11-18 11:31:05 +0530

    fix: null pointers

--- mGit/core/Git.js
+++ mGit/core/Git.js
@@ -9,30 +9,40 @@ function Git (repoName) {
 }
 
 
-function Commit(id, message) {
+function Commit(id, message, prev) {
     this.id = id;
     this.message = message;
+    this.prev = prev;
 }
-
-Git.prototype.GitCommit = function (commit) {
-    if (commit.isPrototypeOf(Commit)) {
-        this.commit = commit;
-        this.prev = this.head;
-    }
-};
 
 Git.prototype.commit = function (message) {
     const newCommitId = Math.random();
-    return (this.head = this.GitCommit(new Commit(newCommitId, message)));
+    return (this.head = (new Commit(newCommitId, message, this.head)));
 };
 
 Git.prototype.log = function () {
     let iterator = this.head;
 
     while (iterator) {
-        const cGist = `${iterator['commit']['id']} 
-                -- ${iterator['commit']['message']}`;
+        const cGist = `${iterator['id']} 
+                -- ${iterator['message']}`;
         console.log(cGist);
         iterator = iterator['prev'];
     }
 };
+
+
+/**
+ * implementation
+ * @type {Git}
+ */
+var repo = new Git('test');
+repo.commit('new commit - 1');
+repo.commit('new commit - 2');
+repo.commit('new commit - 3');
+repo.commit('new commit - 4');
+repo.commit('new commit - 5');
+repo.commit('new commit - 6');
+repo.commit('new commit - 7');
+
+repo.log();
